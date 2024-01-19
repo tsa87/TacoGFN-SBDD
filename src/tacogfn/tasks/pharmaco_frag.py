8c6bf42d30fa4c13629fdf618cfe624463fd60d3
Tony Shen <tsa87@sfu.ca>
2024-01-18 16:48:45 -0800

    exp10

--- src/tacogfn/tasks/pharmaco_frag.py
+++ src/tacogfn/tasks/pharmaco_frag.py
@@ -456,7 +456,7 @@ class PharmacophoreTrainer(StandardOnlineTrainer):
 def main():
     """Example of how this model can be run."""
     hps = {
-        "log_dir": "./logs/20240118-alpha-default-qed-sa-limit-more-nodes",
+        "log_dir": "./logs/20240118-alpha-default-higher-qed-sa-limit",
         "split_file": "dataset/split_by_name.pt",
         "affinity_predictor_path": "model_weights/base_100_per_pocket.pth",
         "pharmacophore_db_path": "misc/pharmacophores_db.lmdb",
@@ -470,7 +470,7 @@ def main():
         "algo": {
             "sampling_tau": 0.99,
             "offline_ratio": 0.0,
-            "max_nodes": 16,
+            "max_nodes": 9,
             "train_random_action_prob": 0.01,
         },
         "cond": {
@@ -486,8 +486,8 @@ def main():
                 "min_docking_score": 0,  # no reward below this
                 "leaky_coefficient": 0.0,
                 "reward_multiplier": 1.0,
-                "max_qed_reward": 0.6,  # no extra reward for qed above this
-                "max_sa_reward": 0.6,  # no extra reward for sa above this
+                "max_qed_reward": 0.7,  # no extra reward for qed above this
+                "max_sa_reward": 0.7,  # no extra reward for sa above this
                 "objectives": ["docking", "qed", "sa"],
             },
         },
